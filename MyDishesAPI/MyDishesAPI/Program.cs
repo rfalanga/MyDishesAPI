0dfe62f2354e781a68c253ccade5950786b3166a
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-18 15:13:01 -0600

    Add DbContext registration and connection string setup
    Updated `Program.cs` to register `MyDishesDbContext` with
    Entity Framework Core and added necessary namespaces.
    Modified `appsettings.json` to include a new "ConnectionStrings"
    section for the SQLite database file "Dishes.db".

--- MyDishesAPI/MyDishesAPI/Program.cs
+++ MyDishesAPI/MyDishesAPI/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MyDishesAPI.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// register the DbContext on the container, getting the connection string from appsettings.json
+builder.Services.AddDbContext<MyDishesDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MyDishesDbContext"))); // Kevin had "ConnectionStrings:DishesDBConnectionString"
 
 var app = builder.Build();
 
