f0a18e4ca578c77b7bee04c4ec3bb3fe80b8d9f3
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-19 06:54:43 -0600

    Beginning to change this for Minimal API call
    but it still is patterned like a MVC call.

--- MyDishesAPI/MyDishesAPI/Program.cs
+++ MyDishesAPI/MyDishesAPI/Program.cs
@@ -15,35 +15,9 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/dishes", async (MyDishesDbContext db) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return await db.Dishes;
 });
 
-// recreate & migrate the database on each run, for demo purposes
-using (var serviceScope = app.Services.GetService<IServiceScopeFactory>()?.CreateScope())
-{
-    var context = serviceScope?.ServiceProvider.GetRequiredService<MyDishesDbContext>();
-    context?.Database.EnsureDeleted();
-    context?.Database.Migrate();
-}
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
