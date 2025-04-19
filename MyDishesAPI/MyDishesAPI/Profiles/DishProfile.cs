5016fd533db6198d25839d7dcfb89f757ea0276e
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-19 09:48:50 -0600

    This is what Kevin has

--- MyDishesAPI/MyDishesAPI/Profiles/DishProfile.cs
+++ MyDishesAPI/MyDishesAPI/Profiles/DishProfile.cs
@@ -8,12 +8,7 @@ namespace MyDishesAPI.Profiles
     {
         public DishProfile()
         {
-            CreateMap<Dish, DishDTO>()
-                .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => src.Ingredients.Select(i => i.Name)))
-                .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Categories.Select(c => c.Name)))
-                .ForMember(dest => dest.DishType, opt => opt.MapFrom(src => src.DishType.Name))
-                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id));
+            CreateMap<Dish, DishDTO>();
         }
     }
 }
