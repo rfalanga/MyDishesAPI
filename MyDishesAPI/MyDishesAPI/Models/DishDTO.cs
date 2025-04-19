ac495945c1f448fc487534c1d656070617f0ec04
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-19 09:39:06 -0600

    Added a Models folder for DTO classes
    then added two DTO classes.

---
+++ MyDishesAPI/MyDishesAPI/Models/DishDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MyDishesAPI.Models
+{
+    public class DishDTO
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ MyDishesAPI/MyDishesAPI/Models/IngredientDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MyDishesAPI.Models
+{
+    // This is an ingredient that belows to a single dish. This will also get rid of the cyclic references.
+    public class IngredientDTO
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public Guid DishId { get; set; }
+    }
+}
