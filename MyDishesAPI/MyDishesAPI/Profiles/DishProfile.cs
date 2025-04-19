f49840d241ce98ddf8c2507fabc3a8b7c799c6c2
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-19 09:47:07 -0600

    This works with AutoMapper
    This was suggested by GH Copilot, but it needs work.

---
+++ MyDishesAPI/MyDishesAPI/Profiles/DishProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MyDishesAPI.Entities;
+using MyDishesAPI.Models;
+
+namespace MyDishesAPI.Profiles
+{
+    public class DishProfile : Profile
+    {
+        public DishProfile()
+        {
+            CreateMap<Dish, DishDTO>()
+                .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => src.Ingredients.Select(i => i.Name)))
+                .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Categories.Select(c => c.Name)))
+                .ForMember(dest => dest.DishType, opt => opt.MapFrom(src => src.DishType.Name))
+                .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
