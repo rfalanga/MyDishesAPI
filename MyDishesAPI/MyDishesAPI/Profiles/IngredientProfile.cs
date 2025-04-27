493291247c86de1e3bbb3e06269c3cbeda08d2c9
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-26 18:32:46 -0600

    Got rid of the IngredientProfile I copied from Kevin

--- MyDishesAPI/MyDishesAPI/Profiles/IngredientProfile.cs
+++ MyDishesAPI/MyDishesAPI/Profiles/IngredientProfile.cs
@@ -2,17 +2,16 @@
 using MyDishesAPI.Entities;
 using MyDishesAPI.Models;
 
-namespace MyDishesAPI.Profiles
+namespace MyDishesAPI.Profiles;
+
+public class IngredientProfile : Profile
 {
-    public class IngredientProfile : Profile
+    public IngredientProfile()
     {
-        public IngredientProfile()
-        {
-            CreateMap<Ingredient, IngredientDTO>()
-                .ForMember(
-                    dest => dest.DishId,
-                    opt => opt.MapFrom(src => src.Dishes.FirstOrDefault().Id)   // Kevin had "src.Dishes.First().Id"
-                );
-        }
+        CreateMap<Ingredient, IngredientDTO>()
+            .ForMember(
+                dest => dest.DishId,
+                opt => opt.MapFrom(src => src.Dishes.FirstOrDefault().Id)   // Kevin had "src.Dishes.First().Id"
+            );
     }
 }
