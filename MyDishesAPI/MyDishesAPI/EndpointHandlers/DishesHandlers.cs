ca00dcb63c0655df514bed5126d004e8b9868374
Rod Falanga <rfalanga@users.noreply.github.com>
2025-05-03 07:26:46 -0600

    Begun converting to using endpoint methods

---
+++ MyDishesAPI/MyDishesAPI/EndpointHandlers/DishesHandlers.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using MyDishesAPI.DbContexts;
+using MyDishesAPI.Models;
+using System.Security.Claims;
+
+namespace MyDishesAPI.EndpointHandlers;
+
+public static class DishesHandlers
+{
+    public static async Task<Ok<IEnumerable<DishDTO>>> GetDishesAsync(MyDishesDbContext db, ClaimsPrincipal claimsPrincipal, IMapper mapper, string? name)
+    {
+        Console.WriteLine($"User: {claimsPrincipal.Identity?.IsAuthenticated}");
+
+        return TypedResults.Ok(mapper.Map<IEnumerable<DishDTO>>(await db.Dishes
+            .Where(d => name == null || d.Name.Contains(name))
+            .ToListAsync()));
+    }
+}
+
