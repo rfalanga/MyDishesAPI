6311c315320c4b35a8b76c5dc1699aa62dfbe52c
Rod Falanga <rfalanga@users.noreply.github.com>
2025-05-06 18:11:40 -0600

    Starting to implement IngredientsHandlers
    generated GH Copilot code #7 #6

---
+++ MyDishesAPI/MyDishesAPI/EndpointHandlers/IngredientsHandlers.cs
@@ -0,0 +1,21 @@
+﻿namespace MyDishesAPI.EndpointHandlers;
+
+public static class IngredientsHandlers
+{
+    public static async Task<Ok<IEnumerable<IngredientDTO>>> GetIngredientsAsync(MyDishesDbContext db, IMapper mapper, string? name)
+    {
+        return TypedResults.Ok(mapper.Map<IEnumerable<IngredientDTO>>(await db.Ingredients
+            .Where(i => name == null || i.Name.Contains(name))
+            .ToListAsync()));
+    }
+    public static async Task<Results<NotFound, Ok<IngredientDTO>>> GetIngredientByIdAsync(MyDishesDbContext db, Guid ingredientId, IMapper mapper)
+    {
+        var ingredient = await db.Ingredients
+            .FirstOrDefaultAsync(i => i.Id == ingredientId);
+        if (ingredient is null)
+        {
+            return TypedResults.NotFound();
+        }
+        return TypedResults.Ok(mapper.Map<IngredientDTO>(ingredient));
+    }
+}
