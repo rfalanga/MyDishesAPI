5040f73ac8a1aa04790b6fe87184d9c873dfcbce
Rod Falanga <rfalanga@users.noreply.github.com>
2025-05-06 18:20:11 -0600

    Added assemblies which weren't added before #6 #7

--- MyDishesAPI/MyDishesAPI/EndpointHandlers/IngredientsHandlers.cs
+++ MyDishesAPI/MyDishesAPI/EndpointHandlers/IngredientsHandlers.cs
@@ -1,14 +1,14 @@
-﻿namespace MyDishesAPI.EndpointHandlers;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using MyDishesAPI.DbContexts;
+using MyDishesAPI.Models;
+
+namespace MyDishesAPI.EndpointHandlers;
 
 public static class IngredientsHandlers
 {
-    public static async Task<Ok<IEnumerable<IngredientDTO>>> GetIngredientsAsync(MyDishesDbContext db, IMapper mapper, string? name)
-    {
-        return TypedResults.Ok(mapper.Map<IEnumerable<IngredientDTO>>(await db.Ingredients
-            .Where(i => name == null || i.Name.Contains(name))
-            .ToListAsync()));
-    }
-    public static async Task<Results<NotFound, Ok<IngredientDTO>>> GetIngredientByIdAsync(MyDishesDbContext db, Guid ingredientId, IMapper mapper)
+    public static async Task<Results<NotFound, Ok<IEnumerable<IngredientDTO>>>> GetIngredientByIdAsync(MyDishesDbContext db, Guid ingredientId, IMapper mapper)
     {
         var ingredient = await db.Ingredients
             .FirstOrDefaultAsync(i => i.Id == ingredientId);
@@ -16,6 +16,6 @@ public static class IngredientsHandlers
         {
             return TypedResults.NotFound();
         }
-        return TypedResults.Ok(mapper.Map<IngredientDTO>(ingredient));
+        return TypedResults.Ok(mapper.Map<IEnumerable<IngredientDTO>>(ingredient));
     }
 }
