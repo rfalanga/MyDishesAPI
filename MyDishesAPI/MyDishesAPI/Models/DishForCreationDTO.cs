f893268889a5730a7a8b955037f375c65536d497
Rod Falanga <rfalanga@users.noreply.github.com>
2025-06-21 17:42:54 -0600

    Modified DishForCreationDTO

--- MyDishesAPI/MyDishesAPI/Models/DishForCreationDTO.cs
+++ MyDishesAPI/MyDishesAPI/Models/DishForCreationDTO.cs
@@ -1,6 +1,10 @@
-﻿namespace MyDishesAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyDishesAPI.Models;
 
 public class DishForCreationDTO
 {
-    public string Name { get; set; } = string.Empty;
+    [Required]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "The name must be between 3 and 100 characters long.")]
+    public required string Name { get; set; } = string.Empty;
 }
