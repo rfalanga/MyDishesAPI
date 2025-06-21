3a62534fe825696e072e4c4939e12a7bfad64daf
Rod Falanga <rfalanga@users.noreply.github.com>
2025-06-21 17:50:55 -0600

    Adding ValidateAnnotationsFilter
    some of this was suggested by GH Copilot

---
+++ MyDishesAPI/MyDishesAPI/EndpointFilters/ValidateAnnotationsFilter.cs
@@ -0,0 +1,18 @@
+﻿using MiniValidation;
+using MyDishesAPI.Models;
+
+namespace MyDishesAPI.EndpointFilters;
+
+public class ValidateAnnotationsFilter : IEndpointFilter
+{
+    public ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        var dishForCreationDTO = context.GetArgument<DishForCreationDTO>(2);
+
+        if (!MiniValidator.TryValidate(dishForCreationDTO, out var validationErrors))
+        {
+            // If validation fails, return a BadRequest with the validation validationErrors
+            return ValueTask.FromResult<object?>(TypedResults.ValidationProblem(validationErrors));
+        }
+    }
+}
